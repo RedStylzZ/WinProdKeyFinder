b909b49e4dbe9c8169d81aaeb15c2d6058577559
RedStylzZ <tizianappler@yahoo.de>
2021-07-09 15:46:06 +0200

    Added CLI Function

---
+++ WinProdKeyFindCLI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using WinProdKeyFind;
+using System.Windows.Forms;
+
+namespace WinProdKeyFindCLI
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string key = KeyDecoder.GetWindowsProductKeyFromRegistry();
+            Console.WriteLine("Key: " + key);
+            Console.WriteLine("Copying to clipboard...");
+            copyToClipboard(key);
+        }
+
+        static private void copyToClipboard(string key)
+        {
+            // set selection (select all)
+            tbWindowsProductKey.SelectionStart = 0;
+            tbWindowsProductKey.SelectionLength = tbWindowsProductKey.Text.Length;
+            // copy selection to the clipboard
+            tbWindowsProductKey.Copy();
+
+
+        }
+    }
+}
